5f534ae4ae54bf3d25d797108ecac2af7faff54d
Meldanor <Bhaals@gmx.de>
2012-08-05 22:16:26 +0200

    Initial commit
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/minestar/castaway/core/CastaAwayCore.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of CastAway.
+ * 
+ * CastAway is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * CastAway is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with CastAway.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.castaway.core;
+
+import de.minestar.minestarlibrary.AbstractCore;
+
+public class CastaAwayCore extends AbstractCore {
+
+    public static final String NAME = "CastAway";
+
+    public CastaAwayCore() {
+        super(NAME);
+    }
+
+}
